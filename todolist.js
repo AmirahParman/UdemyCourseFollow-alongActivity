e729548ab4764e572582b0a1b8222be8e5806e54
Amirah Parman <fthnamirah@hotmail.com>
2023-06-30 16:32:13 +0200

    added a few conditions

--- todolist.js
+++ todolist.js
@@ -1,10 +1,12 @@
 let toDoList = [];
 
-
 while (toDoList !== 'quit') {
     let task = prompt('What would you like to do?');
     if (task == 'new') {
         let toDo = prompt('What would you like to add in your to-do list?');
+        if (toDo == 'quit') {
+            break
+        }
         toDoList.push(toDo);
         console.log(`${toDo} is added to your List`)
     }
@@ -17,14 +19,20 @@ while (toDoList !== 'quit') {
         console.log('*****');
     }
     else if (task == 'delete') {
-        toDoList.pop(prompt('What would you like to delete from the list?'));
+        let deleted = toDoList.pop(prompt('What would you like to delete from the list?'));
+        console.log(`You've deleted ${deleted} from the list`)
     }
     else if (task == 'quit') {
         break;
     }
     else {
         task = prompt('Action invalid. Try again')
+        if ('quit') {
+            break
+        }
     }
 }
 
 console.log("You've exited the program");
+
+
